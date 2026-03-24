682159d7a6c2e6acb2ef555b5f9d24d066545f4d
Kevo2000 <martinezkevinhuerta@gmail.com>
2026-03-23 23:07:32 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'pesaje-kevo-v1';
+const urlsToCache = [
+  './CalcMangKevo.html',
+  './manifest.json',
+  './icono-192x192.png',
+  './icono-512x512.png'
+];
+
+// Instala el Service Worker y guarda los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Intercepta las peticiones para que funcione sin internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Devuelve la versión guardada si no hay internet
+        }
+        return fetch(event.request);
+      })
+  );
+});
